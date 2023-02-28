d1bfb570aa810abaaa5cdd3f03ebc80aa72a9393
zoha.a.qureshi <Eldorado%%9786>
2023-03-01 00:08:00 +0530

    update

--- apiProject/src/App.js
+++ apiProject/src/App.js
@@ -1,31 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const fecthMovieHandler = async () => {
+    setIsLoading(true)
+    setError(null)
+    try {
+      const response = await fetch('https://swapi.dev/api/films');
+      if (!response.ok) {
+        throw new Error("Error Occured")
+      }
+      const data = await response.json();
+
+      const transform = data.results.map(movie => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date
+        }
+      })
+      setMovies(transform);
+      setIsLoading(false)
+    } catch (error) {
+      setError(error.message);
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fecthMovieHandler();
+  }, [])
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fecthMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {isLoading && <h1>Data Loading...</h1>}
+        {(movies.length === 0 && !isLoading && !error) && <h1>No Data to load</h1>}
+        {(error && !isLoading) && <h1>Error Occured</h1>}
+        {(movies.length !== 0 && !isLoading && !error) && <MoviesList movies={movies} />}
       </section>
     </React.Fragment>
   );
