8e0edf31f3ec90f115d6bd2181fb50135067e631
zoha.a.qureshi <Eldorado%%9786>
2023-03-10 16:20:59 +0530

    updates on custom hooks

--- section_15_custom_hooks_01/src/components/NewTask/NewTask.js
+++ section_15_custom_hooks_01/src/components/NewTask/NewTask.js
@@ -1,41 +1,19 @@
-import { useState } from 'react';
+import useApi from '../../hooks/use-Api';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest } = useApi();
+  const createTask = (taskText,dataObj) => {
+    const generatedId = dataObj.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
 
+    props.onAddTask(createdTask);
+  }
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-2b82b-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    sendRequest({url:'https://react-http-2b82b-default-rtdb.firebaseio.com/tasks.json',method: 'POST',body: { text: taskText },
+    headers: {'Content-Type': 'application/json'}},createTask.bind(null,taskText))
   };
 
   return (
