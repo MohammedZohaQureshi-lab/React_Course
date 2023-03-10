372079de966dd40feaa172d4d028c3384fa7830d
zoha.a.qureshi <Eldorado%%9786>
2023-03-10 16:20:16 +0530

    updates on custom hooks

--- section_15_custom_hooks_01/src/App.js
+++ section_15_custom_hooks_01/src/App.js
@@ -1,43 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useApi from './hooks/use-Api';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+
   const [tasks, setTasks] = useState([]);
-
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+  
+  const { isLoading, error, sendRequest: fetchTasks } = useApi();
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const tranformTask = (taskObj) => {
+      const loadedTasks = [];
+  
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+  
+      setTasks(loadedTasks);
+    }
+    fetchTasks({ url: 'https://react-http-2b82b-default-rtdb.firebaseio.com/tasks.json' }, tranformTask);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
