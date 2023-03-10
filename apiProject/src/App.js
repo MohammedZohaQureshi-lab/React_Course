8676cd183c476e3620387f07afddd236995833bc
zoha.a.qureshi <Eldorado%%9786>
2023-03-10 10:13:18 +0530

    updates

--- apiProject/src/App.js
+++ apiProject/src/App.js
@@ -13,22 +13,31 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-http-2b82b-default-rtdb.firebaseio.com/films.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
+      const dataArray=[];
+      for (const key in data) {
+        dataArray.push({
+          id:key,
+          title:data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        })
+      }
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      // const transformedMovies = data.results.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //   };
+      // });
+      setMovies(dataArray);
     } catch (error) {
       setError(error.message);
     }
@@ -39,8 +48,24 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    try {
+      const response = await fetch('https://react-http-2b82b-default-rtdb.firebaseio.com/films.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    });
+    if (!response.ok) {
+      throw new Error('Something went wrong during POST!');
+    }
+    const data = await response.json();
+    console.log(data);
+    } catch (error) {
+      alert(error.message)
+    }
+    
   }
 
   let content = <p>Found no movies.</p>;
