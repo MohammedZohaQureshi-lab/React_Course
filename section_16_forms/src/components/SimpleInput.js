3e7a5cfd86ad9355164956c6d532313bf340020b
zoha.a.qureshi <Eldorado%%9786>
2023-03-12 16:26:30 +0530

    update

--- section_16_forms/src/components/SimpleInput.js
+++ section_16_forms/src/components/SimpleInput.js
@@ -1,12 +1,41 @@
+import { useState } from 'react'
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+  const [inputTouched, setInputTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const validInput = !enteredNameIsValid && inputTouched;
+  const inputHandler = event => {
+    setEnteredName(event.target.value);
+    if (validInput) return;
+  }
+  const inputBlurrHandler = event => {
+    setInputTouched(true);
+    if (validInput) return;
+  }
+
+
+
+  const submitHandler = event => {
+    event.preventDefault();
+    setInputTouched(true);
+    if (!enteredNameIsValid) return;
+    console.log("Submit", enteredName);
+    setEnteredName('');
+    setInputTouched(false);
+  }
+
+  const formClasses = validInput ? 'form-control invalid' : 'form-control';
+
   return (
     <form>
-      <div className='form-control'>
+      <div className={formClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' value={enteredName} id='name' onChange={inputHandler} onBlur={inputBlurrHandler} />
       </div>
+      {validInput && <p className='error-text'>Enter Valid Input</p>}
       <div className="form-actions">
-        <button>Submit</button>
+        <button onClick={submitHandler}>Submit</button>
       </div>
     </form>
   );
