12af23cfa44ff0b561e00b243c8f279cd4c16b71
zoha.a.qureshi <Eldorado%%9786>
2023-03-15 10:12:05 +0530

    redux sample

---
+++ reducSample/redux-demo.js
@@ -0,0 +1,18 @@
+const redux = require('redux');
+const counterReducer = (state = { count: 0 }, action) => {
+    if (action.type === 'INCREMENT') return { count: state.count + 1 };
+    if (action.type === 'DECREMENT') return { count: state.count - 1 }
+    return state
+}
+const store = redux.createStore(counterReducer);
+console.log(store.getState())
+
+const counterComponent = () => {
+    const latestState = store.getState();
+    console.log(latestState)
+}
+
+store.subscribe(counterComponent);
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
