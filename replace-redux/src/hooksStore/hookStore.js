0144f88c8bcf06d60933cfd365d5788f4a11c9f5
zohaqureshi <zzoha9432@gmail.com>
2023-04-09 14:06:52 +0530

    pending code to be added later

--- replace-redux/src/hooksStore/hookStore.js
+++ replace-redux/src/hooksStore/hookStore.js
@@ -1,24 +1,39 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from 'react';
 
 let globalState = {};
 let listeners = [];
-let actions = {}
+let actions = {};
 
-export const useStore = () => {
-    const setState = useState(globalState)[1];
-    const dispatch = (actionIdentifier) => {
-        const newState = actions[actionIdentifier](globalState);
-        globalState = { ...globalState, ...newState };
-        for (const listener of listeners) {
-            listener(globalState)
-        }
+export const useStore = (shouldListen = true) => {
+  const setState = useState(globalState)[1];
+
+  const dispatch = (actionIdentifier, payload) => {
+    const newState = actions[actionIdentifier](globalState, payload);
+    globalState = { ...globalState, ...newState };
+
+    for (const listener of listeners) {
+      listener(globalState);
+    }
+  };
+
+  useEffect(() => {
+    if (shouldListen) {
+      listeners.push(setState);
+    }
+
+    return () => {
+      if (shouldListen) {
+        listeners = listeners.filter(li => li !== setState);
+      }
     };
+  }, [setState, shouldListen]);
 
-    useEffect(() => {
-        listeners.push(setState);
-        return () => {
-            listeners = listeners.filter(listen => listen !== setState)
-        }
-    }, [setState])
-    return [globalState, dispatch]
-}
+  return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+  actions = { ...actions, ...userActions };
+};
