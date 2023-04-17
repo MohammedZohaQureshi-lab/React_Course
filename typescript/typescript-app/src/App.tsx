aecfaf6d10b9062fd5d577f6b947ca3790256aa7
zoha.a.qureshi <Eldorado%%9786>
2023-04-17 14:37:52 +0530

    React with TS

---
+++ typescript/typescript-app/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import ToDos from './components/ToDos';
+import ToDo from './models/todo';
+
+function App() {
+  const todos = [
+    new ToDo('Learn React'),
+    new ToDo('Learn Type Script'),
+    new ToDo('Learn Java Script'),
+  ]
+  return (
+    <div>
+      <h1>React Typescript</h1>
+      <ToDos items={todos} />
+    </div>
+  );
+}
+
+export default App;
